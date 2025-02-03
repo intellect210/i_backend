da29946637e9c5336accab0bd0c705fcc524999e
praveen kumar <94559585+prvn2004@users.noreply.github.com>
2025-02-04 03:37:26 +0530

    edited ecosystem.config.js to run both bullserver and server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,34 @@
 module.exports = {
-    apps : [{
-      name: "i0",  // A name for your application
-      script: "server.js",  // The script file to launch the app
-      instances: "max",  // This can be set to the number of CPU cores to use, or "max" to use all available
-      autorestart: true,  // Automatically restart app if it crashes
-      watch: false,  // Enable/disable watching file changes for automatic restart
-      max_memory_restart: '1G',  // Optional: Restart app if it reaches a memory limit
-      stop_exit_codes: [0],
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-      }
-    }]
-  };
+    apps: [
+        {
+            name: "i0",  
+            script: "server.js",  
+            instances: "max",  
+            autorestart: true,  
+            watch: false,  
+            max_memory_restart: '1G',  
+            stop_exit_codes: [0],
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            }
+        },
+        {
+            name: "bull_worker",
+            script: "bullServer.js",
+            instances: 1, // Typically, Bull workers should run as a single instance to avoid duplicate job processing
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            stop_exit_codes: [0],
+            env: {
+                NODE_ENV: "development",
+            },
+            env_production: {
+                NODE_ENV: "production",
+            }
+        }
+    ]
+};
